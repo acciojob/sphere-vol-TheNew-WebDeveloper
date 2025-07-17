b0d29d7a37dff12f42d6c4bb1e064624dd5eb0d1
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-07-17 18:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+    event.preventDefault();
 	let rad = document.getElementById("radius").value;
+    console.log(rad);
+    
     if(rad < 0) {
         document.getElementById("volume").innerHTML = NaN;
     }
     else {
         let vol = (4/3)*Math.PI*rad*rad*rad;
+        console.log(vol);
+        
         let fixed = parseFloat(vol.toFixed(4));
-        document.getElementById("volume").innerHTML = fixed;
+        console.log(fixed);
+        
+        document.getElementById("volume").value = fixed;
     }
 } 
 
